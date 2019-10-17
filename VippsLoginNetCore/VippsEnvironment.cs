33055a2310b39f557617d53894a55aa39bb1a762
San-jeevan <sanjeevan@outlook.com>
2019-10-17 21:17:05 +0200

    public class

--- VippsLoginNetCore/VippsEnvironment.cs
+++ VippsLoginNetCore/VippsEnvironment.cs
@@ -2,7 +2,7 @@
 
 namespace VippsLoginNetCore
 {
-    static class VippsEnvironment
+    public static class VippsEnvironment
     {
         public static string Prod = "https://api.vipps.no/access-management-1.0/access";
         public static string Test = "https://apitest.vipps.no/access-management-1.0/access";
