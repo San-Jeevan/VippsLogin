3ad30563057741b304c6a80c446950c0fbddefca
andrem0 <andregjoh@gmail.com>
2019-12-03 16:51:42 +0100

    Update VippsModels.cs
    id_token is string, not int

--- VippsLogin461/VippsModels.cs
+++ VippsLogin461/VippsModels.cs
@@ -52,7 +52,7 @@ namespace VippsLogin461
     {
         public string access_token { get; set; }
         public int expires_in { get; set; }
-        public int id_token { get; set; }
+        public string id_token { get; set; }
         public string scope { get; set; }
         public string token_type { get; set; }
     }
